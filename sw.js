6f42297b5653a4a9123a537ce43519500ae958a2
dcdigital <dsdiegocoelho@gmail.com>
2026-06-12 23:17:57 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'arenaflow-v9';
+const CACHE_NAME = 'arenaflow-v10';
 const ASSETS = [
   '/', '/index.html', '/css/styles.css',
   '/js/firebase-config.js', '/js/app.js', '/manifest.json'
