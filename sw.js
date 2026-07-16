991d6f09202166a32cd84cda957c46ac64a4a9c4
dcdigital <dsdiegocoelho@gmail.com>
2026-07-15 21:13:57 -0300

    Update CACHE_NAME to 'arenaflow-v21'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'arenaflow-v20';
+const CACHE_NAME = 'arenaflow-v21';
 const ASSETS = [
   './', './index.html', './css/styles.css',
   './js/firebase-config.js', './js/app.js', './manifest.json',
